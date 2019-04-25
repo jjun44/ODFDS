16569624442b8991064e7bb787dafc8b4573a943
Jaeho <jaeho.jun4@gmail.com>
2019-04-24 19:09:53 -0700

    Updated socketApi.js

--- controllers/socketApi.js
+++ controllers/socketApi.js
@@ -1,3 +1,11 @@
+/**
+ * socketApi.js
+ *
+ * 4/21/2019
+ * CS160 - ODFDS Project
+ * Socket API that controlls all socekt connections.
+ */
+
 var socket_io = require('socket.io');
 var io = socket_io();
 var socketApi = {};
@@ -5,13 +13,17 @@ var socketApi = {};
 socketApi.io = io;
 
 io.on('connection', function(socket){
-  console.log('A user connected');
+  console.log(socket.id, 'A user connected');
+  socket.on('disconnect', function () {
+    console.log('A user disconnected');
+  });
 });
 
-socketApi.driverInfo = function(dName, dPhone, distance, arrivesIn) {
-  console.log(dName, dPhone, distance, arrivesIn);
+socketApi.sendDeliveryInfo = function(dName, dPhone, distance, arrivesIn, orderID) {
+  console.log('Sending delivery information to a restaurant user...');
   io.sockets.emit('driverFound', { dName: dName, dPhone: dPhone,
-                                     distance: distance, arrivesIn: arrivesIn });
+                                   distance: distance, arrivesIn: arrivesIn,
+                                   orderID: orderID });
 }
 
 module.exports = socketApi;
